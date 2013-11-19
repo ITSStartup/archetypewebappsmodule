3d31cd2d7b016a9e1aae46008ffde80ce5a2818a
Camilo Lopes <camilo@camilolopes.com.br>
2013-11-19 16:43:40 -0200

    fixed dependency module web

---
+++ src/main/resources/archetype-resources/webapps-web/src/main/java/br/com/its/web/controller/ExampleController.java
@@ -0,0 +1,14 @@
+#set( $symbol_pound = '#' )
+#set( $symbol_dollar = '$' )
+#set( $symbol_escape = '\' )
+package br.com.its.web.controller;
+
+import javax.ws.rs.Path;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+@Path("/example")
+public class ExampleController {
+
+}
